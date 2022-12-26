51719fe4085fe021236879db6c7c95fa87f3895a
Christos Pitsoulakis <105457831+ChristosPts@users.noreply.github.com>
2022-12-26 17:57:08 +0200

    Update projects.js

--- js/projects.js
+++ js/projects.js
@@ -16,7 +16,7 @@ const projectsArray = [
         projectDesc: "A search engine built with Google's Custom Search API ",
         displayImgs: ["/portfolio/img/projectimgs/mojito1.jpg","/portfolio/img/projectimgs/mojito2.jpg","/portfolio/img/projectimgs/mojito3.jpg","/portfolio/img/projectimgs/mojito4.jpg"],
 
-        link1:"https://mojito-hypb202k9-christospts.vercel.app/",
+        link1:"https://mojito-five.vercel.app/",
         link2:"https://github.com/ChristosPts/Mojito-Search-Engine",
     },
     {
